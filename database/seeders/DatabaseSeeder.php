c602e4df27a74146ec9aa573f259e85a5a6e21b9
mdshahco <mdshahcoding.gmail.com>
2023-09-02 19:17:35 +0600

    seeding random data with factory and faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,8 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call(ProductSeeder::class);
+        \App\Models\User::factory(10)->create();
+        \App\Models\Product::factory(50)->create();
+
+        // $this->call(ProductSeeder::class);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
